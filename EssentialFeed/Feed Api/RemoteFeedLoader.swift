4c73818ec286cb780d3337e1e8dbedc4bf9bbb4e
Jhean Carlos Pineros Diaz <jpineros@veritran.com>
2022-02-06 14:58:15 -0500

    Delivers ítems array on 200 HTTP response with JSON ítems

--- EssentialFeed/Feed Api/RemoteFeedLoader.swift
+++ EssentialFeed/Feed Api/RemoteFeedLoader.swift
@@ -43,8 +43,8 @@ public final class RemoteFeedLoader {
             client.get(from: url) { result in
                 switch result {
                 case let .success(data, _):
-                    if let _ = try? JSONSerialization.jsonObject(with: data){
-                        completion(.success([]))
+                    if let root = try? JSONDecoder().decode(Root.self, from: data){
+                        completion(.success(root.items))
                     } else{
                         completion(.failure(.invalidData))
                     }
@@ -54,4 +54,9 @@ public final class RemoteFeedLoader {
             }
     }
     
+}
+
+
+private struct Root: Decodable {
+    let items: [FeedItem]
 }
