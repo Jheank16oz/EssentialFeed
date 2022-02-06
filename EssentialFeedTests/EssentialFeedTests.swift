1f9ef0d4500e9c5c3395cde9fb8e5ac298469f10
Jhean Carlos Pineros Diaz <jpineros@veritran.com>
2022-02-06 10:03:47 -0500

    Replace optional params with enum with success/failure cases

--- EssentialFeedTests/EssentialFeedTests.swift
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -70,14 +70,14 @@ class EssentialFeedTests: XCTestCase {
         var requestedURLs : [URL]{
             return messages.map{ $0.url }
         }
-        var messages = [(url:URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
+        var messages = [(url:URL, completion: (HTTPClientResult) -> Void)]()
         
-        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
             messages.append((url,completion))
         }
         
         func complete(with error: Error, at index: Int = 0){
-            messages[index].completion(error, nil)
+            messages[index].completion(.failure(error))
         }
         
         func complete(withStatusCode code: Int, at index: Int = 0){
@@ -86,8 +86,8 @@ class EssentialFeedTests: XCTestCase {
                 statusCode: code,
                 httpVersion: nil,
                 headerFields: nil
-            )
-            messages[index].completion(nil, response)
+            )!
+            messages[index].completion(.success(response))
         }
     }
 
