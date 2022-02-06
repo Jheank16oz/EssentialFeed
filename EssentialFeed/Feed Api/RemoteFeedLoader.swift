4c70cbf2ca9a2044d4fce3633e436832d65c2873
Jhean Carlos Pineros Diaz <jpineros@veritran.com>
2022-02-06 09:22:44 -0500

    Delivers invaliData error on non 200 HTTP responses

--- EssentialFeed/Feed Api/RemoteFeedLoader.swift
+++ EssentialFeed/Feed Api/RemoteFeedLoader.swift
@@ -10,7 +10,7 @@ import Foundation
 
 public protocol HTTPClient {
     
-    func get(from url: URL, completion: @escaping (Error) -> Void)
+    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
     
 }
 
@@ -20,6 +20,7 @@ public final class RemoteFeedLoader {
    
     public enum Error: Swift.Error {
             case connectivity
+            case invalidData
     }
     
     public init(url: URL, client:HTTPClient){
@@ -28,8 +29,13 @@ public final class RemoteFeedLoader {
     }
     
     public func load(completion: @escaping (Error) -> Void) {
-            client.get(from: url) { error in
-                completion(.connectivity)
+            client.get(from: url) { error, response in
+                
+                if response != nil {
+                    completion(.invalidData)
+                } else {
+                    completion(.connectivity)
+                }
             }
     }
     
--- EssentialFeedTests/EssentialFeedTests.swift
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -42,6 +42,17 @@ class EssentialFeedTests: XCTestCase {
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
     
+    func test_load_deliversErrorOnNonHTTPResponse(){
+        let (sut, client) = makeSUT()
+
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load {  capturedErrors.append($0) }
+        
+        client.complete(withStatusCode: 400)
+        XCTAssertEqual(capturedErrors, [.invalidData])
+    }
+    
+    
     // MARK: - Helpers
     private func makeSUT(url:URL = URL(string:"http://a-given-url.com")!) ->(sut:
                                                                         RemoteFeedLoader, client:HTTPCLientSpy){
@@ -55,14 +66,24 @@ class EssentialFeedTests: XCTestCase {
         var requestedURLs : [URL]{
             return messages.map{ $0.url }
         }
-        var messages = [(url:URL, completion: (Error) -> Void)]()
+        var messages = [(url:URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
         
-        func get(from url: URL, completion: @escaping (Error) -> Void) {
+        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
             messages.append((url,completion))
         }
         
         func complete(with error: Error, at index: Int = 0){
-            messages[index].completion(error)
+            messages[index].completion(error, nil)
+        }
+        
+        func complete(withStatusCode code: Int, at index: Int = 0){
+            let response = HTTPURLResponse(
+                url: requestedURLs[index],
+                statusCode: code,
+                httpVersion: nil,
+                headerFields: nil
+            )
+            messages[index].completion(nil, response)
         }
     }
 
