12125a666b28adb380cff478d29e568ffa22a623
Jhean Carlos Pineros Diaz <jpineros@veritran.com>
2022-02-06 09:30:58 -0500

    And more test assertions for different HTTP status code

--- EssentialFeedTests/EssentialFeedTests.swift
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -44,12 +44,16 @@ class EssentialFeedTests: XCTestCase {
     
     func test_load_deliversErrorOnNonHTTPResponse(){
         let (sut, client) = makeSUT()
-
-        var capturedErrors = [RemoteFeedLoader.Error]()
-        sut.load {  capturedErrors.append($0) }
         
-        client.complete(withStatusCode: 400)
-        XCTAssertEqual(capturedErrors, [.invalidData])
+        let samples = [199, 201, 300, 400, 500]
+        samples.enumerated().forEach{ index, code in
+            var capturedErrors = [RemoteFeedLoader.Error]()
+            sut.load {  capturedErrors.append($0) }
+            
+            client.complete(withStatusCode: code, at: index)
+            XCTAssertEqual(capturedErrors, [.invalidData])
+        }
+        
     }
     
     
