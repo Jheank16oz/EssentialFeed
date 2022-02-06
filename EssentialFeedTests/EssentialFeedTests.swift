6b2a07a33832722a13eeaab35aed5e48140bed8d
Jhean Carlos Pineros Diaz <jpineros@veritran.com>
2022-02-06 15:48:39 -0500

    use explicit JSON data for non HTTP response tests

--- EssentialFeedTests/EssentialFeedTests.swift
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -47,7 +47,8 @@ class EssentialFeedTests: XCTestCase {
         let samples = [199, 201, 300, 400, 500]
         samples.enumerated().forEach{ index, code in
             expect(sut, toCompleteWith: .failure(.invalidData), when: {
-                client.complete(withStatusCode: code, at: index)
+                let json = makeItemsJSON([])
+                client.complete(withStatusCode: code, data : json, at: index)
             })
         }
     }
@@ -145,7 +146,7 @@ class EssentialFeedTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0){
+        func complete(withStatusCode code: Int, data: Data, at index: Int = 0){
             let response = HTTPURLResponse(
                 url: requestedURLs[index],
                 statusCode: code,
