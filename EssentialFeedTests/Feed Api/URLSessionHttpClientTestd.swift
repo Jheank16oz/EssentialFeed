b52264a7eb61065f88e8f277b55d50e43842f879
Jhean Carlos Pineros Diaz <jpineros@veritran.com>
2022-02-16 08:52:24 -0500

    guarantees correct URL and HTTP method  on get requests

--- EssentialFeedTests/Feed Api/URLSessionHttpClientTestd.swift
+++ EssentialFeedTests/Feed Api/URLSessionHttpClientTestd.swift
@@ -29,6 +29,21 @@ class URLSessionHTTPClient {
 
 class URLSessionHTTPClientTests: XCTestCase {
     
+    func test_getFromURL_performsGETRequestWithURL(){
+        URLProtocolStub.startInterceptingRequest()
+        
+        let exp = expectation(description: "wait for request")
+        let url = URL(string: "http://any-url.com")!
+        URLProtocolStub.observeRequests { request in
+            XCTAssertEqual(request.url, url)
+            XCTAssertEqual(request.httpMethod, "GET")
+            exp.fulfill()
+        }
+        URLSessionHTTPClient().get(from: url){ _ in }
+        wait(for: [exp], timeout: 1.0)
+        URLProtocolStub.stopInterceptingRequest()
+    }
+    
     func test_getFromURL_failsOnRequesError(){
         URLProtocolStub.startInterceptingRequest()
         let url = URL(string: "http://any-url.com")!
@@ -55,6 +70,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     private class URLProtocolStub:URLProtocol {
         private static var stub:Stub?
+        private static var requestObserver:((URLRequest) -> Void)?
         
         private struct Stub{
             let data: Data?
@@ -69,13 +85,19 @@ class URLSessionHTTPClientTests: XCTestCase {
         static func stopInterceptingRequest(){
             URLProtocol.unregisterClass(URLProtocolStub.self)
             stub = nil
+            requestObserver = nil
         }
         
         static func stub(data: Data?, response: URLResponse?, error:Error? = nil){
             stub = Stub(data:data, response:response, error: error)
         }
+        
+        static func observeRequests(observer: @escaping (URLRequest) -> Void){
+            requestObserver = observer
+        }
 
         override class func canInit(with request: URLRequest) -> Bool {
+            requestObserver?(request)
             return true
         }
         
