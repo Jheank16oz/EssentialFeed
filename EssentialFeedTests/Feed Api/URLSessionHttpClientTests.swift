b017b45f7883b67caef5ff5a01d3f7abe680189e
Jhean Carlos Pineros Diaz <jpineros@veritran.com>
2022-02-19 09:21:16 -0500

    Move ´URLSessionHTTPClient´ to its own file in production

--- EssentialFeedTests/Feed Api/URLSessionHttpClientTests.swift
+++ EssentialFeedTests/Feed Api/URLSessionHttpClientTests.swift
@@ -1,28 +1,6 @@
 import XCTest
 import EssentialFeed
 
-class URLSessionHTTPClient:HTTPClient {
-    private let session: URLSession
-    
-    init(session: URLSession = .shared) {
-        self.session = session
-    }
-    
-    struct UnexpectedValuesRepresentation: Error {}
-    
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-        session.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-            } else if let data = data, let response = response as? HTTPURLResponse {
-                completion(.success(data, response))
-            } else {
-                completion(.failure(UnexpectedValuesRepresentation()))
-            }
-        }.resume()
-    }
-}
-
 class URLSessionHTTPClientTests: XCTestCase {
     
     override func setUp() {
@@ -98,7 +76,7 @@ class URLSessionHTTPClientTests: XCTestCase {
 
     // MARK: - Helpers
     
-    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> HTTPClient {
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> URLSessionHTTPClient {
         let sut = URLSessionHTTPClient()
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
